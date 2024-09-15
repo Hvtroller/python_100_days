c5ec0c4cafc8fb26e9e3761532ccd2e128b51d7e
dongvv-2538 <vu.viet.dong@sun-asterisk.com>
2024-09-15 08:37:26 +0700

    day 22: detect collision

--- day_22/project/main.py
+++ day_22/project/main.py
@@ -25,4 +25,13 @@ while game_is_on:
   screen.update()
   pong.move()
 
+  if pong.ycor() > 280 or pong.ycor() < -280:
+    pong.bounce_y()
+
+  if (pong.distance(r_paddle) < 50 and pong.xcor() > 320) or (pong.distance(l_paddle) < 50 and pong.xcor() < -320):
+    pong.bounce_x()
+
+  if pong.xcor() > 380 or pong.xcor() < -380:
+    pong.reset_position()
+
 screen.exitonclick()
