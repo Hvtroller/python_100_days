04d21410de959d534f6f97117ce9db86426724bb
dongvv-2538 <vu.viet.dong@sun-asterisk.com>
2024-09-15 10:14:03 +0700

    day 24: add highest score

--- day_20/project/scoreboard.py
+++ day_20/project/scoreboard.py
@@ -6,6 +6,7 @@ class Scoreboard(Turtle):
   def __init__(self):
     super().__init__()
     self.score = 0
+    self.highest_score = 0
     self.color("white")
     self.hideturtle()
     self.penup()
@@ -18,7 +19,13 @@ class Scoreboard(Turtle):
 
   def update_scoreboard(self):
     self.clear()
-    self.write(f"Score: {self.score}", align=ALIGMENT, font=FONT)
+    self.write(f"Score: {self.score} | High score: {self.highest_score}", align=ALIGMENT, font=FONT)
+
+  def reset(self):
+    if self.score > self.highest_score:
+      self.highest_score = self.score
+    self.score = 0
+    self.update_scoreboard()
 
   def game_over(self):
     self.goto(0, 0)
