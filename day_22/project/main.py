72f68972905890b4593cf28158c66169bc42b9a8
dongvv-2538 <vu.viet.dong@sun-asterisk.com>
2024-09-15 08:26:26 +0700

    day 22: add ball

--- day_22/project/main.py
+++ day_22/project/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from pong import Pong
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600, startx=0, starty=0)
@@ -8,6 +10,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+pong = Pong()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,6 +21,8 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+  time.sleep(0.1)
   screen.update()
+  pong.move()
 
 screen.exitonclick()
